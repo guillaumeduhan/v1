699665d8d4035818d4ada9bf2d3f7eff0b8b967f
Guillaume Duhan <myhappyagency@gmail.com>
2020-05-07 23:02:54 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="flex">
+    <Main
+      description="As a Front-End developer since 10 years now, I enjoy creating beautiful apps on Javascript Frameworks."
+      name="Guillaume Duhan."
+      title="Creating web apps for start-ups."
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Main from "./components/Main.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Main
   }
-}
+};
 </script>
 
-<style>
+<style lang="css">
+@import url("https://fonts.googleapis.com/css?family=Jost:300,400,500,600,700&display=swap");
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Jost;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
