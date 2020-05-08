80583ae66fe038444e68191c01a45f80fea6ba5b
Guillaume Duhan <myhappyagency@gmail.com>
2020-05-08 12:32:49 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,35 @@
 <template>
-  <div id="app" class="flex">
+  <div id="app" class="flex flex-col">
     <Main
       description="As a Front-End developer since 10 years now, I enjoy creating beautiful apps on Javascript Frameworks."
       name="Guillaume Duhan."
-      title="Creating web apps for start-ups."
+      :social="links"
+      title="Creating web apps."
     />
+    <Articles title="I write articles on JavaScript." />
   </div>
 </template>
 
 <script>
+import Articles from "./components/Articles.vue";
 import Main from "./components/Main.vue";
 
 export default {
   name: "App",
   components: {
+    Articles,
     Main
+  },
+  data() {
+    return {
+      links: {
+        facebook: "https://www.facebook.com/groups/javascript.paris",
+        twitter: "https://www.facebook.com/groups/javascript.paris",
+        instagram: "https://www.facebook.com/groups/javascript.paris",
+        youtube: "https://www.facebook.com/groups/javascript.paris",
+        github: "https://www.facebook.com/groups/javascript.paris"
+      }
+    };
   }
 };
 </script>
